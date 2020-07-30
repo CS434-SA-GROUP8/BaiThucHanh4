ff6caeac9fa267486a9b2d1f38f7d042bd4f718d
ADMIN <ADMIN@192.168.1.6>
2020-07-30 19:30:42 +0700

    Trần Đình Tin

---
+++ CVHV/src/HCN/CHVHCN.java
@@ -0,0 +1,16 @@
+package HCN;
+import java.util.Scanner;
+public class CHVHCN {
+	public static void main (String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào chiều dài của hình chữ nhật:");
+        double length = scanner.nextDouble();
+        System.out.println("Nhập vào chiều rộng của hình chữ nhật:");
+        double width = scanner.nextDouble();
+        double area = length*width;
+        System.out.println("Diện tích hình chữ nhật là:"+area);
+        System.out.println("------------------------------------------");
+        System.out.println("Chương trình này được đăng tại Freetuts.net");
+    }
+}
